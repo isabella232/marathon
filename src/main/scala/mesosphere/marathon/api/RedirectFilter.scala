4f81285b4983a961109fd36a48d035a421d7ad83
Brenden Matthews <brenden@diddyinc.com>
2013-09-26 09:35:14 -0700

    Fixed web UI redirection by proxy.
    Instead of redirecting web requests to the other master, we'll just
    proxy everything.

--- src/main/scala/mesosphere/marathon/api/RedirectFilter.scala
+++ src/main/scala/mesosphere/marathon/api/RedirectFilter.scala
@@ -1,6 +1,9 @@
 package mesosphere.marathon.api
 
+import scala.collection.JavaConverters._
 import com.google.inject.Inject
+import java.net.{HttpURLConnection, URL}
+import java.io.{OutputStreamWriter, InputStream}
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.logging.Logger
 import javax.inject.Named
@@ -18,27 +21,73 @@ class RedirectFilter @Inject()
 
   def init(filterConfig: FilterConfig) {}
 
+  def copy(input: InputStream, output: OutputStreamWriter) = {
+    val bytes = new Array[Byte](1024)
+    Iterator
+      .continually(input.read(bytes))
+      .takeWhile(-1 !=)
+      .foreach(read => output.write(new String(bytes, 0, read), 0, read))
+  }
+
   def doFilter(rawRequest: ServletRequest,
                rawResponse: ServletResponse,
                chain: FilterChain) {
-    //TODO(FL): Think about proxying instead of redirecting.
-    if (rawRequest.isInstanceOf[HttpServletRequest]) {
-      val request = rawRequest.asInstanceOf[HttpServletRequest]
-      val leaderData = schedulerService.getLeader
-      if (schedulerService.isLeader) {
-        chain.doFilter(request, rawResponse)
-      } else {
-        log.info("Redirecting request.")
+    try {
+      if (rawRequest.isInstanceOf[HttpServletRequest]) {
+        val request = rawRequest.asInstanceOf[HttpServletRequest]
+        val leaderData = schedulerService.getLeader
         val response = rawResponse.asInstanceOf[HttpServletResponse]
 
-        val newUrl = "http://%s%s".format(leaderData.get,
-          request.getRequestURI)
+        if (schedulerService.isLeader) {
+          chain.doFilter(request, response)
+        } else {
+          log.info("Proxying request.")
 
-        log.info("Redirect header sent to point to: " + newUrl)
+          val method = request.getMethod
 
-        response.setHeader("Location", newUrl )
-        response.setStatus(HttpServletResponse.SC_TEMPORARY_REDIRECT)
+          val proxy =
+            new URL("http://%s%s".format(leaderData.get, request.getRequestURI))
+              .openConnection().asInstanceOf[HttpURLConnection]
+
+
+          val names = request.getHeaderNames
+          // getHeaderNames() and getHeaders() are known to return null, see:
+          // http://docs.oracle.com/javaee/6/api/javax/servlet/http/HttpServletRequest.html#getHeaders(java.lang.String)
+          if (names != null) {
+            for (name <- names.asScala) {
+              val values = request.getHeaders(name)
+              if (values != null) {
+                proxy.setRequestProperty(name, values.asScala.mkString(","))
+              }
+            }
+          }
+          proxy.setDoOutput(method == "POST")
+          proxy.setRequestMethod(method)
+
+          method match {
+            case "POST" =>
+              val proxyWriter =
+                new OutputStreamWriter(proxy.getOutputStream)
+              val input = request.getInputStream
+              copy(input, proxyWriter)
+              proxyWriter.close
+            case _ =>
+          }
+
+          val proxyReader = proxy.getInputStream
+
+          response.setStatus(proxy.getResponseCode())
+
+          val responseWriter =
+            new OutputStreamWriter(response.getOutputStream)
+
+          copy(proxyReader, responseWriter)
+          proxyReader.close
+          responseWriter.close
+        }
       }
+    } catch {
+      case t: Throwable => log.warning("Exception while proxying: " + t)
     }
   }
 
